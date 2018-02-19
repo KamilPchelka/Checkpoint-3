8f3516a7d8e57521014cc82fd45c93d2dc68bf40
siemaeniu500 <siemaeniu500@gmail.com>
2018-02-19 13:05:53 +0100

    Finished 4 requirments.

--- src/main/java/com/codecool/kamilpchelka/checkpoint3/controllers/MainMenuController.java
+++ src/main/java/com/codecool/kamilpchelka/checkpoint3/controllers/MainMenuController.java
@@ -4,11 +4,14 @@ import com.codecool.kamilpchelka.checkpoint3.database.AuthorsDAO;
 import com.codecool.kamilpchelka.checkpoint3.database.BookTypesDAO;
 import com.codecool.kamilpchelka.checkpoint3.database.BooksDAO;
 import com.codecool.kamilpchelka.checkpoint3.database.PublishersDAO;
+import com.codecool.kamilpchelka.checkpoint3.models.Author;
 import com.codecool.kamilpchelka.checkpoint3.models.Book;
 import com.codecool.kamilpchelka.checkpoint3.views.MainMenuView;
 
 import java.sql.SQLException;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class MainMenuController {
 
@@ -48,18 +51,48 @@ public class MainMenuController {
             case "1":
                 runAddNewBookCreator();
                 break;
+            case "2":
+                runExistingBookEditor();
+                break;
             case "3":
                 runDeleteBookProcess();
                 break;
             case "5":
                 runShowingBooksListByTheirNamesAscending();
                 break;
+            case "6":
+                runShowingBooksListByGivenAuthor();
+                break;
         }
+    }
+
+    private void runExistingBookEditor() {
+
+    }
+
+    private void runShowingBooksListByGivenAuthor() throws SQLException {
+
+        List<Book> books = booksDAO.getAllBooks();
+
+        List<Author> authors = authorsDAO.getAuthors();
+
+        mainMenuView.printAuthors(authors);
+
+        int authorID = Integer.parseInt(mainMenuView.getUserInput("Type author id: "));
+
+        mainMenuView.printBooks(books.stream().filter(book -> book.getAuthor() == authorID).collect(Collectors.toList()));
+
+        mainMenuView.getUserInput();
     }
 
     private void runShowingBooksListByTheirNamesAscending() throws SQLException {
 
         List<Book> books = booksDAO.getAllBooks();
+
+        Collections.sort(books, (b1, b2) -> (b1.getTitle().compareTo(b2.getTitle())));
+
+        mainMenuView.printBooks(books);
+        mainMenuView.getUserInput();
 
 
     }
@@ -76,6 +109,12 @@ public class MainMenuController {
 
     private void runAddNewBookCreator() throws SQLException {
 
+        booksDAO.addOrUpdateBook(createBookObjectFromUserInput());
+
+        mainMenuView.showNewbookAddedSuccesfully();
+    }
+
+    private Book createBookObjectFromUserInput() throws SQLException {
         int isbn = Integer.parseInt(mainMenuView.getUserInput("Type ISBN: "));
 
         mainMenuView.printAuthors(authorsDAO.getAuthors());
@@ -98,9 +137,7 @@ public class MainMenuController {
 
         Book book = new Book(isbn, author, title, publisher, publicationYear, price, type);
 
-        booksDAO.addOrUpdateBook(book);
-
-        mainMenuView.showNewbookAddedSuccesfully();
+        return book;
     }
 
 }
