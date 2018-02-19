9ff7194231031b9f171b7bceb49160f5a23ac3eb
siemaeniu500 <siemaeniu500@gmail.com>
2018-02-19 10:04:12 +0100

    Update URL to database.

--- src/main/java/com/codecool/kamilpchelka/checkpoint3/database/AbstractDAO.java
+++ src/main/java/com/codecool/kamilpchelka/checkpoint3/database/AbstractDAO.java
@@ -12,7 +12,7 @@ public abstract class AbstractDAO implements Database {
     public void connectToDatabase() throws SQLException, ClassNotFoundException {
 
         Class.forName("net.sf.log4jdbc.DriverSpy");
-        connection = DriverManager.getConnection("jdbc:log4jdbc:sqlite:quest_store.db?foreign_keys=on;");
+        connection = DriverManager.getConnection("jdbc:log4jdbc:sqlite:database.db?foreign_keys=on;");
         connection.setAutoCommit(true);
     }
 
